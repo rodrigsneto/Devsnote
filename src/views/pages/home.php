64282ca3e6dbecc157cc378f4449cdd9da327f01
Rodrigues Neto <rodriguesneto95@outlook.com>
2022-07-30 14:01:29 -0300

    Partials Feeds

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -8,20 +8,8 @@
         <div class="row">
             <div class="column pr-5">
 
-                <div class="box feed-new">
-                    <div class="box-body">
-                        <div class="feed-new-editor m-10 row">
-                            <div class="feed-new-avatar">
-                                <img src="<?=$base?>/media/avatars/avatar.jpg" />
-                            </div>
-                            <div class="feed-new-input-placeholder">O que você está pensando, <?=$loggedUser->name?>?</div>
-                            <div class="feed-new-input" contenteditable="true"></div>
-                            <div class="feed-new-send">
-                                <img src="<?=$base?>/assets/images/send.png" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+            <?= $render('feed-editor', ['loggedUser' => $loggedUser]) ;?>
 
             <?= $render('feed-item', ['loggedUser' => $loggedUser]) ;?>
 
